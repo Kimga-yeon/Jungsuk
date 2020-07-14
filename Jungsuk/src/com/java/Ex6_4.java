797c6796a24517526681ec00d74bc262b0bba3c6
김가연 <alice2301@naver.com>
2020-07-14 22:00:46 +0900

    07-14

---
+++ Jungsuk/src/com/java/Ex6_4.java
@@ -0,0 +1,19 @@
+package com.java;
+
+public class Ex6_4 {
+    public static void main (String[] args) {
+        
+        int x = 0;
+        int y =0;
+        int result = add(3,11);
+        System.out.println(result);
+    }
+    
+    
+    
+    static int add(int x, int y) {
+        int result = x + y;
+        return result;
+    }
+}
+
---
+++ Jungsuk/src/com/java/Ex6_6.java
@@ -0,0 +1,26 @@
+package com.java;
+class Data { int x; }
+
+// 기본형 매개변수
+public class Ex6_6 {
+    public static void main(String[] args) {
+        Data d = new Data();
+        d.x=10;
+        System.out.println("main() : x = " + d.x);
+        
+        change(d.x);
+        
+        System.out.println("After change(d.x)");
+        System.out.println("main() : x " + d.x);
+        
+        
+    }
+
+static void change(int x) {
+    x=1000;
+    System.out.println("change() : x = " + x );
+}
+
+
+}
+
