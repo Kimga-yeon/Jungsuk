83d2fbf855ff6f139d7bc8848261f55e52548258
김가연 <alice2301@naver.com>
2020-07-23 21:49:06 +0900

    07-23

--- Jungsuk/src/com/java/Ex7_3.java
+++ Jungsuk/src/com/java/Ex7_3.java
@@ -14,6 +14,9 @@ class Child2 extends Parent2{
     
     void method() {
         System.out.println("x =" +x);
+        
+         
+        
         System.out.println("this.x =" + this.x);
         System.out.println("super.x =" + super.x);
     }
