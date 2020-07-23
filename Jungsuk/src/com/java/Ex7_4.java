7e96b1cc26de2d982e99d5dcc1f082ca4c4d43fd
김가연 <alice2301@naver.com>
2020-07-23 21:54:55 +0900

    07-23

--- Jungsuk/src/com/java/Ex7_4.java
+++ Jungsuk/src/com/java/Ex7_4.java
@@ -4,9 +4,11 @@ public class Ex7_4 {
     public static void main(String[] args) {
         Time t = new Time();
 //        t.hour(-100);
+        
         t.setHour(21);
         System.out.println(t.getHour());
         t.setHour(100);
+        System.out.println(t.getHour());
     }
 }
 
@@ -21,9 +23,13 @@ class Time{
         this.hour = hour;
     }
         // 매개변수로 넘겨진 hour가 유효한지 확인해서 알려주는 메소드
-        private boolean isValidHour(int hour) {
+     
+    
+    private boolean isValidHour(int hour) {
             return hour < 0 || hour > 23;
         }
+    
+    
         public int getHour () {return hour;};
         
  }
