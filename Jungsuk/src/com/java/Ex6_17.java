5242cb393e1e8b9b16ceaa2d09be6c1e7e23b77d
Kimga-yeon <alice2301@naver.com>
2020-07-27 23:32:12 +0900

    07-27

---
+++ Jungsuk/src/com/java/Ex6_17.java
@@ -0,0 +1,34 @@
+package com.java;
+
+public class Ex6_17 {
+    
+    public static int[] shuffle(int[] arr) {
+        
+        
+        return result;
+    }
+
+    
+    
+    public static void main(String[] args) {
+        
+        int a = 1;
+        int b = 2;
+        int x = 3;
+        
+        int x = int a;
+        
+        
+        
+        
+        
+        
+        int[] original = {1,2,3,4,5,6,7,8,9};
+        System.out.println(java.util.Arrays.toString(original));
+        
+        int[] result = shuffle(original);
+        
+    }
+}
+
+
